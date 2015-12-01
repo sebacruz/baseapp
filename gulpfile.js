8b007a72697dd136f0332255b7e9cf284f77021f
Sebastián Cruz <cruzjesussebastian@gmail.com>
2015-12-01 11:54:02 -0300

    Updating gulpfile to match the airbnb standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
-var pkg  = require('./package.json'),
-    manifest = require('asset-builder')('./manifest.json');
+/* eslint-env node, es6 */
 
-var argv = require('yargs')
+const manifest = require('asset-builder')('./manifest.json');
+
+const argv = require('yargs')
   .default('production', false)
   .boolean('production', 'disable-fail')
   .argv;
 
-var gulp = require('gulp'),
-    merge = require('merge-stream'),
-    lazypipe = require('lazypipe'),
-    runSequence = require('run-sequence'),
-    gulpif = require('gulp-if'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    eslint = require('gulp-eslint'),
-    less = require('gulp-less'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    imagemin = require('gulp-imagemin'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const merge = require('merge-stream');
+const lazypipe = require('lazypipe');
+const runSequence = require('run-sequence');
+const gulpif = require('gulp-if');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const eslint = require('gulp-eslint');
+const less = require('gulp-less');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 
 // CLI options
-var enabled = {
+const enabled = {
   // Enable static asset revisioning when `--production`
   rev: argv.production,
 
@@ -40,8 +41,8 @@ var enabled = {
   failJSLint: (argv.disableFail !== undefined) ? argv.disableFail : argv.production,
 
   // Strip debug statments from javascript when `--production`
-  stripJSDebug: argv.production
-}
+  stripJSDebug: argv.production,
+};
 
 
 /**
@@ -50,36 +51,36 @@ var enabled = {
  * @param  {string} filename
  * @return {object}
  */
-function cssTasks(filename){
+function cssTasks(filename) {
   return lazypipe()
-    .pipe(function(){
-      return gulpif(enabled.maps, sourcemaps.init())
+    .pipe(function() {
+      return gulpif(enabled.maps, sourcemaps.init());
     })
-    .pipe(function(){
+    .pipe(function() {
       return gulpif('*.less', less({
-        paths: ['bower_components']
-      }))
+        paths: ['bower_components'],
+      }));
     })
-    .pipe(function(){
-      return gulpif('*.scss', sass())
+    .pipe(function() {
+      return gulpif('*.scss', sass());
     })
     .pipe(concat, filename)
-    .pipe(function(){
+    .pipe(function() {
       return gulpif(enabled.minify, cssnano({
         discardComments: {
-          removeAllButFirst: true
+          removeAllButFirst: true,
         },
         autoprefixer: {
           browsers: ['last 3 versions'],
-          cascade: false
-        }
-      }))
+          cascade: false,
+        },
+      }));
     })
     .pipe(function() {
       return gulpif(enabled.maps, sourcemaps.write('.', {
-        sourceMappingURLPrefix: '/assets/css/'
-      }))
-    })()
+        sourceMappingURLPrefix: '/assets/css/',
+      }));
+    })();
 }
 
 /**
@@ -88,41 +89,41 @@ function cssTasks(filename){
  * @param  {string} filename
  * @return {object}
  */
-function jsTasks(filename){
+function jsTasks(filename) {
   return lazypipe()
     .pipe(function() {
-      return gulpif(enabled.maps, sourcemaps.init())
+      return gulpif(enabled.maps, sourcemaps.init());
     })
     .pipe(concat, filename)
     .pipe(uglify, {
       compress: {
-        drop_console: enabled.stripJSDebug
+        drop_console: enabled.stripJSDebug,
       },
       output: {
         beautify: !enabled.minify,
-        ascii_only: true
-      }
+        ascii_only: true,
+      },
     })
     .pipe(function() {
       return gulpif(enabled.maps, sourcemaps.write('.', {
-        sourceMappingURLPrefix: '/assets/js/'
-      }))
-    })()
+        sourceMappingURLPrefix: '/assets/js/',
+      }));
+    })();
 }
 
 /**
  * Run lossless compression on images
  */
-function imgTasks(){
+function imgTasks() {
   return lazypipe()
     .pipe(imagemin, {
       progressive: true,
       interlaced: true,
       svgoPlugins: [
         {removeUnknownsAndDefaults: false},
-        {cleanupIDs: false}
-      ]
-    })()
+        {cleanupIDs: false},
+      ],
+    })();
 }
 
 /**
@@ -131,57 +132,57 @@ function imgTasks(){
  * @param  {string} directory
  * @return {object}
  */
-function writeFile(directory){
+function writeFile(directory) {
   return lazypipe()
-    .pipe(gulp.dest, manifest.paths.dist + directory)()
+    .pipe(gulp.dest, manifest.paths.dist + directory)();
 }
 
 
 /**
  * Lints configuration JSON and project JS
  */
-gulp.task('lint:js', function(){
+gulp.task('lint:js', function() {
   return gulp.src(manifest.getProjectGlobs().js)
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(gulpif(enabled.failJSLint, eslint.failOnError()))
-})
+    .pipe(gulpif(enabled.failJSLint, eslint.failOnError()));
+});
 
 /**
  * Deletes the build folder entirely
  */
-gulp.task('clean', require('del').bind(null, [manifest.paths.dist]))
+gulp.task('clean', require('del').bind(null, [manifest.paths.dist]));
 
 /**
  * Scripts compilation
  *
  * Run the JS taks for each javascript file specified in the manifest file
  */
-gulp.task("compile:js", ['lint:js'], function(){
-  var merged = merge()
+gulp.task('compile:js', ['lint:js'], function() {
+  const merged = merge();
 
-  manifest.forEachDependency('js', function(dep){
+  manifest.forEachDependency('js', function(dep) {
     merged.add(
       gulp.src(dep.globs)
         .pipe(jsTasks(dep.name))
-    )
-  })
+    );
+  });
 
   return merged
     .pipe(writeFile('js'))
-    .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
 
 /**
  * CSS compilation
  *
  * Run the CSS taks for each css file specified in the manifest file
  */
-gulp.task('compile:css', function(){
-  var merged = merge()
+gulp.task('compile:css', function() {
+  const merged = merge();
 
-  manifest.forEachDependency('css', function(dep){
-    var cssTasksInstance = cssTasks(dep.name);
+  manifest.forEachDependency('css', function(dep) {
+    const cssTasksInstance = cssTasks(dep.name);
 
     if (!enabled.failStyleTask) {
       cssTasksInstance.on('error', function(err) {
@@ -193,32 +194,32 @@ gulp.task('compile:css', function(){
     merged.add(
       gulp.src(dep.globs)
         .pipe(cssTasks(dep.name))
-    )
-  })
+    );
+  });
 
   return merged
     .pipe(writeFile('css'))
-    .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
 
 /**
  * Images optimization
  */
-gulp.task('publish:img', function(){
+gulp.task('publish:img', function() {
   return gulp.src(manifest.globs.images)
     .pipe(imgTasks())
     .pipe(writeFile('img'))
-    .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
 
 /**
  * Fonts publishing
  */
-gulp.task('publish:fonts', function(){
-  var merged = merge()
+gulp.task('publish:fonts', function() {
+  const merged = merge();
 
-  manifest.globs.fonts.map(function(file){
-    var base = manifest.paths.source;
+  manifest.globs.fonts.map(function(file) {
+    let base = manifest.paths.source;
 
     if (file.indexOf('bower_components') >= 0) {
       base = 'bower_components';
@@ -226,13 +227,13 @@ gulp.task('publish:fonts', function(){
 
     merged.add(
       gulp.src([file], {base: base})
-    )
-  })
+    );
+  });
 
   return merged
     .pipe(writeFile('fonts'))
-    .pipe(browserSync.stream())
-})
+    .pipe(browserSync.stream());
+});
 
 /**
  * Random vendor stuff publising
@@ -242,22 +243,22 @@ gulp.task('publish:fonts', function(){
  *
  * @todo TODO find a better way to do this in the same way as the font files
  */
-gulp.task('publish:misc', function(){
+gulp.task('publish:misc', function() {
   // we put the file list here because if you put them in the manifest.json file
   // the asset-builder module automatically adds the paths.source to each path
-  var files = [
+  const files = [
   ];
 
   return gulp.src(files, {base: 'bower_components'})
-    .pipe(writeFile('vendor'))
-})
+    .pipe(writeFile('vendor'));
+});
 
 /**
  * Run all the build tasks with an clean up beforehand
  */
-gulp.task('build', ['clean'], function(callback){
-  runSequence('compile:js', 'compile:css', ['publish:img', 'publish:fonts'], callback)
-})
+gulp.task('build', ['clean'], function(callback) {
+  runSequence('compile:js', 'compile:css', ['publish:img', 'publish:fonts'], callback);
+});
 
 /**
  * Use BrowserSync to proxy your dev server and synchronize code changes
@@ -268,39 +269,39 @@ gulp.task('build', ['clean'], function(callback){
  *
  * See: http://www.browsersync.io
  */
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   browserSync.init({
-    proxy: manifest.config.devUrl
-  })
+    proxy: manifest.config.devUrl,
+  });
 
   gulp.watch([
-      manifest.paths.source + 'backend/css/**/*',
-      manifest.paths.source + 'frontend/css/**/*',
-      manifest.paths.source + 'backend/less/**/*',
-      manifest.paths.source + 'frontnend/less/**/*'
-    ], ['compile:css'])
+    manifest.paths.source + 'backend/css/**/*',
+    manifest.paths.source + 'frontend/css/**/*',
+    manifest.paths.source + 'backend/less/**/*',
+    manifest.paths.source + 'frontnend/less/**/*',
+  ], ['compile:css']);
 
   gulp.watch([
-      manifest.paths.source + 'backend/js/**/*',
-      manifest.paths.source + 'frontend/js/**/*'
-    ], ['compile:js'])
+    manifest.paths.source + 'backend/js/**/*',
+    manifest.paths.source + 'frontend/js/**/*',
+  ], ['compile:js']);
 
   gulp.watch([
-      manifest.paths.source + 'backend/images/**/*',
-      manifest.paths.source + 'frontend/images/**/*',
-    ], ['publish:img'])
+    manifest.paths.source + 'backend/images/**/*',
+    manifest.paths.source + 'frontend/images/**/*',
+  ], ['publish:img']);
 
   gulp.watch([
-      manifest.paths.source + 'backend/fonts/**/*',
-      manifest.paths.source + 'frontend/fonts/**/*'
-    ], ['publish:fonts'])
+    manifest.paths.source + 'backend/fonts/**/*',
+    manifest.paths.source + 'frontend/fonts/**/*',
+  ], ['publish:fonts']);
 
-  gulp.watch(['./manifest.json'], ['build'])
-})
+  gulp.watch(['./manifest.json'], ['build']);
+});
 
 /**
  * Alias of build taks
  */
-gulp.task('default', function(){
-  gulp.start('build')
-})
+gulp.task('default', function() {
+  gulp.start('build');
+});
